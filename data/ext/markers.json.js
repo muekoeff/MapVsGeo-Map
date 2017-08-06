5e326dbf6b893ab81e0a44e12a367cac348d4d87
nw520 <me@nw520.de>
2017-08-06 20:22:13 +0200

    Init

---
+++ data/ext/markers.json.js
@@ -0,0 +1,78 @@
+var markers = {
+	"6ilw12": {
+		data: {
+			displayName: "Berlin, Germany",
+			means: [
+				"s-bahn",
+				"subway"
+			],
+			position: {
+				lat: 52.507629,
+				lng: 13.1459661
+			}
+		},
+		meta: {
+			adder: "naseweis520"
+		},
+		reddit: {
+			author: "vinnivinnivinni",
+			commentsId: "6ilw12"
+		}
+	},
+	"6k2qd5": {
+		data: {
+			displayName: "Barcelona, Spain",
+			means: [
+				"metro"
+			],
+			position: {
+				lat: 41.3948976,
+				lng: 2.0787279
+			}
+		},
+		meta: {
+			adder: "naseweis520"
+		},
+		reddit: {
+			author: "sergilazaro",
+			commentsId: "6k2qd5"
+		}
+	},
+	"6k6tv9": {
+		data: {
+			displayName: "Vienna, Austria",
+			means: [
+				"subway"
+			],
+			position: {
+				lat: 48.2208286,
+				lng: 16.2399765
+			}
+		},
+		meta: {
+			adder: "naseweis520"
+		},
+		reddit: {
+			author: "p6788",
+			commentsId: "6k6tv9",
+			poster: "pm_me_your_biography"
+		}
+	},
+	"6k84a9": {
+		data: {
+			displayName: "Hong Kong",
+			means: [],
+			position: {
+				lat: 22.3595967,
+				lng: 114.0007825
+			}
+		},
+		meta: {
+			adder: "naseweis520"
+		},
+		reddit: {
+			author: "barrettxav",
+			commentsId: "6k84a9"
+		}
+	}
+};
